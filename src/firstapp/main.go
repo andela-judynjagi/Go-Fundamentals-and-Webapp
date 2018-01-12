f42925140da1bf0fdf0c9620b6c940a6a89077f8
Judy Njangi <judy.njagi@andela.com>
2018-01-12 16:59:44 +0300

    declaring and initiliazing variables

--- src/firstapp/main.go
+++ src/firstapp/main.go
@@ -5,6 +5,13 @@ import (
   "runtime"
 )
 
+var (
+  name, course string
+  module float64
+)
+
 func main(){
   fmt.Println("Hello from", runtime.GOOS)
+  fmt.Println("My name is", name)
+  fmt.Println("Module is set to", module)
 }

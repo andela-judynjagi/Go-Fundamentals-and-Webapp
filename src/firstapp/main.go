6d1db342430af39ec73a35850537433377baeafb
Judy Njangi <judy.njagi@andela.com>
2018-01-12 17:13:45 +0300

    determining types

--- src/firstapp/main.go
+++ src/firstapp/main.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "runtime"
+  "reflect"
 )
 
 var (
@@ -10,8 +11,24 @@ var (
   module float64
 )
 
+// Go can infer types as long as they are initialized eg it knows name is of type string
+var (
+  fullname = "Judy Njagi"
+  mycourse = "Learning Go"
+  mymodule = 13.60
+)
+
+// Go is strongly typed eg we can't add a +b directly. Instead you type to convert eg a into type of int then add
+var (
+  a = 13.600
+  b = 2
+)
+
 func main(){
   fmt.Println("Hello from", runtime.GOOS)
-  fmt.Println("My name is", name)
-  fmt.Println("Module is set to", module)
+  fmt.Println("My name is", name, "and is of type", reflect.TypeOf(name))
+  fmt.Println("My name is", fullname, "and is of type", reflect.TypeOf(fullname))
+  fmt.Println("Module is set to", module, "and is of type", reflect.TypeOf(module))
+  c := int(a) + b
+  fmt.Println("a+b = ", c)
 }
